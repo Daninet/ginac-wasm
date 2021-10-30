aa05d58a5b698b88665246880fdf756f4f97f827
Dani Biró <danibiro93@gmail.com>
2021-10-30 11:03:12 +0300

    Build demo frontend

--- demo/webpack/webpack.common.js
+++ demo/webpack/webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, '..', './build'),
+    path: path.resolve(__dirname, '..', './dist'),
     filename: 'bundle.js',
   },
   plugins: [
