33522f4b76a1161ccc37d0810001c855daaa10b6
Dani Biró <danibiro93@gmail.com>
2021-10-31 14:13:10 +0200

    Add github badge

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -14,7 +14,12 @@ export const App = () => {
 
   return (
     <div style={{ margin: 30 }}>
-      <h2>GiNaC-WASM demo</h2>
+      <h2>
+        GiNaC-WASM demo &nbsp;
+        <a href="https://github.com/Daninet/ginac-wasm" target="_blank" rel="noreferrer">
+          <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/daninet/ginac-wasm?style=social" />
+        </a>
+      </h2>
       {ready ? <Calculator /> : 'Loading...'}
     </div>
   );
